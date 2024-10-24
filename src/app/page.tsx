8ff02cddf90a6a81cb5b206c5698e823c111d5e9
sandeshtreeleaf <sandesh.khadka.treeleaf@gmail.com>
2024-10-24 16:45:00 +0545

    fix state errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,181 @@
-import Image from "next/image";
-
+"use client"
+import Janus, { JanusJS } from "janus-gateway";
+import { useEffect, useState } from "react";
+import { json } from "stream/consumers";
+import adapter from "webrtc-adapter";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [initialized, setInitialized] = useState(false)
+  const [janusHandle, setJanusHandle] = useState<Janus>()
+  const [pluginHandle, setPluginHandle] = useState<JanusJS.PluginHandle>()
+  const [inRoom, setInRoom] = useState(false)
+  const [isPublishing, setIsPublishing] = useState(false)
+  const [isRecording, setIsRecording] = useState(false)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function getPluginHandle() {
+    console.log("Accessing plugin handle: ", pluginHandle)
+    return pluginHandle;
+  }
+  useEffect(() => {
+    Janus.init({
+      debug: true,
+      dependencies: Janus.useDefaultDependencies(
+        {
+          adapter: adapter
+        }
+      ), // or: Janus.useOldDependencies() to get the behaviour of previous Janus versions
+      callback: function () {
+        console.log("initialized")
+        setInitialized(true)
+        let janus = new Janus({
+          server: "https://janus.anydone.net/janus",
+          // server: "http://localhost:8088/janus",
+          // iceServers: [
+          // { urls: "stun:stun.l.google.com:19302" },
+          // ],
+          success: function () {
+            setJanusHandle(janus)
+            console.log("Janus session created: ", janus.getSessionId())
+          }
+
+        })
+      }
+    });
+  }, [Janus])
+  function attachPlugin() {
+    let localHandle: JanusJS.PluginHandle;
+    if (!janusHandle) {
+      alert("No janus session")
+      return;
+    }
+    janusHandle.attach({
+      plugin: "janus.plugin.videoroom",
+      error: function (error) {
+        alert(`Error:  ${error}`)
+      },
+      success: function (handle) {
+        localHandle = handle
+        setPluginHandle(localHandle)
+      },
+      onmessage: function (message, jsep) {
+        console.log("message: ", message)
+        if (jsep) {
+          if (localHandle == undefined) {
+            alert("No plugin handle, failed to handle remote jsep")
+            return;
+          }
+          localHandle.handleRemoteJsep({
+            jsep: jsep,
+            success: function (data) {
+              console.log("Handle Remote Jsep success: ", data)
+            }
+          })
+
+        }
+      }
+    })
+  }
+  function joinRoom() {
+    if (!pluginHandle) {
+      alert("Plugin not attached")
+      return;
+    }
+    pluginHandle.send({
+      message: {
+        request: "join",
+        ptype: "publisher",
+        room: 1234
+      },
+      success: function (data) {
+        console.log("Joined room: ", data)
+        setInRoom(true)
+      }
+    })
+  }
+
+  function startPublishing() {
+    if (!pluginHandle) {
+      alert("No plugin handle")
+      return;
+    }
+    pluginHandle.createOffer({
+      tracks: [
+        { type: "video", capture: true },
+        { type: "audio", capture: true }
+      ],
+      success: function (jsep) {
+        pluginHandle.webrtcStuff.mySdp = {
+          type: jsep.type || "",
+          sdp: jsep.sdp || ""
+        }
+        pluginHandle.send({
+          message: {
+            request: "publish",
+            display: "sandesh",
+          },
+          jsep: jsep
+        })
+
+        setIsPublishing(true)
+      }
+    })
+  }
+
+  function startRecording() {
+    console.log("Staring Recording")
+    if (!pluginHandle) {
+      alert("No plugin handle")
+      return
+    }
+    pluginHandle.send({
+      message: {
+        "request": "enable_recording",
+        "room": 1234,
+        "record": true,
+        "start_timestamp": Date.now()
+      }
+    })
+    setIsRecording(true)
+  }
+  function stopRecording() {
+    console.log("Stopping Recording")
+    if (!pluginHandle) {
+      alert("No plugin handle")
+      return
+    }
+    pluginHandle.send({
+      message: {
+        "request": "enable_recording",
+        "room": 1234,
+        "record": false,
+        "start_timestamp": Date.now()
+      }
+    })
+    setIsRecording(false)
+  }
+  return (
+    <div>
+      {initialized ?
+        <div>
+          <div> {JSON.stringify(pluginHandle)} </div>
+          <button onClick={attachPlugin} className="border border-black p-3"> Attach plugin </button>
+          {pluginHandle ? <div>
+            <button onClick={joinRoom} className="border p-3 border-black"> Join room 1234 </button>
+            {inRoom ? <div>
+              <div>Room id: {1234}</div>
+              <div>
+                <button className="border border-black p-3" onClick={startPublishing}> Publish</button>
+              </div>
+              {isPublishing ?
+                <div>
+                  {isRecording ? <button className="border border-black p-3" onClick={stopRecording}>Stop Record</button> : <button className="border border-black p-3" onClick={startRecording}> Start Record</button>}
+
+                </div>
+                : <div> Not Publishing</div>}
+            </div> : "Not in room"}
+          </div> : "Plugin not attached"}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        : <div>Not initialized</div>}
     </div>
   );
 }
+
